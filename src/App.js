d4e39c5b46225bb60b1f4bf42dbb1980bc4e90ca
adrien-dubois <mcsnoos@gmail.com>
2022-01-18 23:54:36 +0100

    [WIP] Info section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import GlobalStyle from "./globalStyles";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar } from "./components";
+import Home from "./pages/HomePage/Home";
 
 function App() {
   return (
     <Router>
       <GlobalStyle />
       <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+      </Routes>
     </Router>
   );
 }
---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,13 @@
+export const homeObjOne = {
+    lightBg: false,
+    primary: true,
+    lightBg: false, 
+    imgStart: '', 
+    lightTopLine: true, 
+    lightTextDesc: true, 
+    buttonLabel: 'Get started', 
+    description: 'Veniam ullamco eu nisi culpa id irure laboris non irure pariatur dolore aliquip.', 
+    headline: 'Nouvelle génération de spécialistes du développement Web', 
+    lightText: true, 
+    topLine:  'Collectif de Freelance'
+}
